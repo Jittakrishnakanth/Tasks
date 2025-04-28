28738a734ebefd0bb153ad1de12259899df3bf1f
Krishnakanth Jitta <168550218+Jittakrishnakanth@users.noreply.github.com>
2025-04-28 16:03:32 +0530

    Add files via upload

---
+++ ColorPicker.js
@@ -0,0 +1,22 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("selectedColorHexCode");
+ function Kohli() {
+    document.body.style.backgroundColor = "#e0e0e0";
+    button5.textContent = "#e0e0e0";
+}
+
+function Rohit() {
+    document.body.style.backgroundColor = "#6fcf97";
+    button5.textContent = "#6fcf97";
+}
+function Dhoni() {
+    document.body.style.backgroundColor = "#56ccf2";
+    button5.textContent = "#56ccf2";
+}
+function Raina() {
+    document.body.style.backgroundColor = "#bb6bd9";
+    button5.textContent = "#bb6bd9";
+}
